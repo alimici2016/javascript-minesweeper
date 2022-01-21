ead1bc7dfd909d533ca51aa616d05e5592022836
Hamsa Alimci <alimici2016@gmail.com>
2022-01-21 05:53:07 -0600

    With a double for loop I am able to find the cell that contains the asterisk

--- minesweeper.js
+++ minesweeper.js
@@ -4,5 +4,15 @@
 //
 
 export const annotate = (input) => {
+  //Double loop to target the cell with asterisk within multidimensional array
+  for (let row = 0; row < input.length; row++){
+    for (let column = 0; column < input[0].length; column++){
+      //when the index of the asterisk is located console.log
+      if(arr[row][column] === "*"){
+
+        console.log('this is ast', arr[row][column])
+      }
+    }
+  }
   throw new Error('Remove this statement and implement this function');
 };
